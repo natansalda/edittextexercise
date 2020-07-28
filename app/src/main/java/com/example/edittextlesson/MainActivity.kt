e0f40b431448acdbbe267cc6bde076b599c9c776
Natalia Nazaruk <nazarjazz@wp.pl>
2020-07-28 14:12:57 +0200

    XML for exercise created

--- app/src/main/java/com/example/edittextlesson/MainActivity.kt
+++ app/src/main/java/com/example/edittextlesson/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.edittextlesson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /* create function for result button clicked
+    1. create function
+    2. take text from all 3 edit text - you may want to create variables for that
+    3. set text for result text
+    4. make result text visible
+    5. hide keyboard
+     */
+
+    // from: https://stackoverflow.com/a/17789187/7586488
+    private fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
